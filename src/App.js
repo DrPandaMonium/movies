714458b9aab3d9d4362513240c64a361bfa6894a
DrPandaMonium <105460824+DrPandaMonium@users.noreply.github.com>
2023-10-04 16:45:16 -0500

    Movie

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import MovieCard from './MovieCard';
+
+import './App.css';
+import SearchIcon from './search.svg';
+
+// apiKey = 82768798
+const API_URL = 'http://www.omdbapi.com?apikey=82768798'
+
+const movie1 = {
+    "Title": "Spiderman and Grandma",
+    "Year": "2009",
+    "imdbID": "tt1433184",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMjE3Mzg0MjAxMl5BMl5BanBnXkFtZTcwNjIyODg5Mg@@._V1_SX300.jpg"
+}
+
+const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    // async means it will take time to fetch data
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        setMovies(data.Search);
+    }
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            searchMovies(searchTerm);
+        }
+    }
+
+    useEffect(() => {
+        searchMovies('avengers')
+    }, [])
+
+    return (
+        <div className='app'>
+            <h1>MOVIES</h1>
+
+            <div className='search'>
+                <input 
+                    placeholder='Search for movies'
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    onKeyDown={handleKeyDown}
+                />
+                <img 
+                    src={SearchIcon}
+                    alt='search'
+                    onClick={() => searchMovies(searchTerm)}
+                />
+            </div>
+
+            {movies?.length > 0 
+                ? (
+                    <div className='container'>
+                        {movies.map((movie) => (
+                            <MovieCard movie={movie} />
+                        ))}
+                    </div>
+                )
+                : (
+                    <div className='empty'>
+                        <h2>No Movies Found</h2>
+                    </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/MovieCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+// JSX file is same as JS but its good for recognizing components
+// good practice to name the component the same as the jsx file
+const MovieCard = ({ movie }) => {
+    return (
+        <div className='movie'>
+            <div>
+                <p>{movie.Year}</p>
+            </div>
+
+            <div>
+                <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.Title} />
+            </div>
+
+            <div>
+                <span>{movie.Type}</span>
+                <h3>{movie.Title}</h3>
+            </div>
+        </div>
+    );
+}
+
+export default MovieCard;
